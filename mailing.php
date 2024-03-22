6482f2774b2bcc315736364d18d45252859719b3
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2024-03-22 12:01:29 +0000

    new commit 2

--- mailing.php
+++ mailing.php
@@ -7,9 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home - Kreatyve Designs</title>
     <meta name="description" content="(C) 2016 - 2024 Kreatyve Designs. All Right Reserved">
-    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/Merriweather.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/Open%20Sans.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
     <link rel="stylesheet" href="{{ asset('css/baguetteBox.min.css') }}">
@@ -18,7 +15,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+        <link href="resources/css/baguetteBox.min.css" rel="stylesheet"/>
+        <link href="resources/css/Merriweather.css" rel="stylesheet"/>
+        <link href="resources/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+
+
 
 
 <head>
