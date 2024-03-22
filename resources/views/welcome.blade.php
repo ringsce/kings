92fc1247b36eed180de22eadd6014d086707f986
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2024-03-22 11:42:15 +0000

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,6 +133,7 @@
             <a href="#"><i class="fab fa-facebook-f"></i></a>
             <a href="#"><i class="fab fa-twitter"></i></a>
             <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i>Liberapay</i></a>
         </div>
     </div>
 
