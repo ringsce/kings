f9612717f9464e1ddaad218e7389ded11da30c93
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2024-12-07 14:59:09 +0000

    adding .kayte to apache2

--- footer.php
+++ footer.php
@@ -1,37 +1,46 @@
-<footer>
-    <div class="footer-content">
-        <!-- Logo -->
-        <div class="footer-logo">
-            <img src="resources/img/ringsce.png" alt="Logo">
-        </div>
-        <!-- Copyright notice -->
-        <div class="footer-copyright">
-            &copy; <?php echo date("Y"); ?>  Kreatyve Designs. All rights reserved.
-        </div>
-        <!-- Three columns -->
-        <div class="footer-columns">
-            <div class="footer-column">
-                <h3>About Us</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div class="footer-column">
-                <h3>Services</h3>
-                <ul>
-                    <li>Service 1</li>
-                    <li>Service 2</li>
-                    <li>Service 3</li>
-                </ul>
-            </div>
-            <div class="footer-column">
-                <h3>Contact Us</h3>
-                <p>Email: info@example.com</p>
-                <p>Phone: 123-456-7890</p>
-            </div>
-        </div>
-    </div>
-</footer>
+footer {
+    div(class="footer-content") {
+        // Logo
+        div(class="footer-logo") {
+            img(src="resources/img/ringsce.png", alt="Logo")
+        }
 
-<script>
+        // Copyright notice
+        div(class="footer-copyright") {
+            "&copy; " + system.date("Y") + " Kreatyve Designs. All rights reserved."
+        }
+
+        // Three columns
+        div(class="footer-columns") {
+            // About Us Column
+            div(class="footer-column") {
+                h3("About Us")
+                p("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+            }
+
+            // Services Column
+            div(class="footer-column") {
+                h3("Services")
+                ul {
+                    li("Service 1")
+                    li("Service 2")
+                    li("Service 3")
+                }
+            }
+
+            // Contact Us Column
+            div(class="footer-column") {
+                h3("Contact Us")
+                p("Email: info@example.com")
+                p("Phone: 123-456-7890")
+            }
+        }
+    }
+}
+
+// JavaScript Section
+js {
+    """
     // Toggle menu function
     document.getElementById('menuToggle').addEventListener('click', function() {
         var menuContainer = document.getElementById('menuContainer');
@@ -41,9 +50,8 @@
             menuContainer.style.left = '-200px';
         }
     });
-</script>
+    """
+}
 
-<!-- Font Awesome CDN -->
-<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-</body>
-</html>
+// Font Awesome CDN
+js(src="https://kit.fontawesome.com/a076d05399.js", crossorigin="anonymous")
